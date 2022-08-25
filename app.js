d8f9371953f99d7803a8ceb2df4d35e065cd18d1
B3ccaM <89497608+B3ccaM@users.noreply.github.com>
2022-08-25 18:13:34 -0500

    2.4 update
    2.4 assignment

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
-console.log("Hello World!");
-document.write(3 + 4);
+let life = 100; // life bar
+let name = "Dante"; // character name STRING
+
+/* ^ Global variables are listed above ^ */
+
+life = life - 50; // damage taken MATH
+
+{
+    let life = 80; // block variable
+}
+
+const person = {age:"40's", sex:"male", species: "half-demon"};
+
+console.log("Life"); // changed output
+/*document.write(3 + 4); ignore this as it's from previous 1.6 assignment */
