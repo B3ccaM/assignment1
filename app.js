64df8dad6417560b1e6f5776968ad4c5c6d09942
B3ccaM <89497608+B3ccaM@users.noreply.github.com>
2022-08-26 08:57:25 -0500

    Update app.js

--- app.js
+++ app.js
@@ -12,4 +12,3 @@ life = life - 50; // damage taken MATH
 const person = {age:"40's", sex:"male", species: "half-demon"};
 
 console.log("Life"); // changed output
-/*document.write(3 + 4); ignore this as it's from previous 1.6 assignment */
